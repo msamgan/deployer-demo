2957a46ac894e7178e95a07837b032cf36521b44
msamgan <mohdsamgankhan@gmail.com>
2024-07-01 19:50:04 -0400

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div class="min-h-screen flex flex-col items-center justify-center">
+            <h1 class="text-6xl font-bold">msamgan</h1>
+            <p class="text-lg mt-4">A fellow software engineer in learning</p>
+            <p class="text-lg mt-4">
+                We are having a deployer demo here....
+            </p>
+        </div>
+    </body>
+</html>
